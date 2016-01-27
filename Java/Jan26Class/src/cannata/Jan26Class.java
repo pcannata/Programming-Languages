73c1dda350af0fd3f622966a3cbd70685e4e2c0a
Phil Cannata <phil.cannata@oracle.com>
2016-01-27 11:25:10 -0600

    Updated from Jan 26 class

---
+++ Java/Jan26Class/src/cannata/Jan26Class.java
@@ -0,0 +1,12 @@
+package cannata;
+
+public class Jan26Class {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        int i = 4;
+        String s = "some string";
+        System.out.println("\tsome\" " + i + "\nstr\\ing");
+    }
+
+}
